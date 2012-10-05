1254f3f801227138f4d6fa08c5c486d16fba0caa
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2012-10-05 13:05:16 +1000

    removed

--- app/mailers/admin_mailer.rb
+++
@@ -1,8 +0,0 @@
-class AdminMailer < ActionMailer::Base
-  default from: "myasnikovdaniil@gmail.com"
-  
-  def new_user_waiting_for_approval(user)
-    @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
-  end
-end
