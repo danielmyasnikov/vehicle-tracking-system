eba229aa91ded885251192f8e5626dadd4e24ed4
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2012-10-05 15:02:34 +1000

    removed emailing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  after_create :send_admin_mail
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me,
                   :approved
 
   def active_for_authentication? 
     super && approved? 
-  end 
-  
-  def send_admin_mail
-    AdminMailer.new_user_waiting_for_approval(self).deliver
   end
 
   def inactive_message 
