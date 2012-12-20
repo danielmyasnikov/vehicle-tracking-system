0446d2a48a0361fff406bfac497b0dcfc1bc4591
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2012-12-20 13:22:21 +1100

    added condition to migration

--- db/migrate/20121219125204_add_pirmary_key_id_to_email_notifications.rb
+++ db/migrate/20121219125204_add_pirmary_key_id_to_email_notifications.rb
@@ -1,5 +1,5 @@
 class AddPirmaryKeyIdToEmailNotifications < ActiveRecord::Migration
   def change
-    add_column :email_notifications, :id, :primary_key
+    add_column :email_notifications, :id, :primary_key unless EmailNotification.column_names.include?('id')
   end
 end
