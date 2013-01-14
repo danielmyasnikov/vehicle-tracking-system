b8bb2110748a623dcb03438de974de0102bec6d4
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-01-14 12:42:27 +1100

    styled reports

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,7 +1,6 @@
 class ReportController < ApplicationController
   def index
     @fleets = Fleet.where(:truck_fleet_id => current_user.truck_fleet_id)
-    
     respond_to do |format|
       format.html
     end
