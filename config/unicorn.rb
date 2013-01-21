9207f1fa8c5cafbf8dae5122397ef8b68c7c0f74
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-01-21 12:01:35 +1100

    changed listening socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid "/home/dan/www/mytruckfleet/tmp/pids/unicorn.pid"
 stperr_path "/home/dan/www/mytruckfleet/log/unicorn.log"
 stdout_path "/home/dan/www/mytruckfleet/log/unicorn.log"
 
-listen 3000
+listen "/tmp/unicorn.todo.sock"
 worker_processes 2
 timeout 30
