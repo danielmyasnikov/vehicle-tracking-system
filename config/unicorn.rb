350a57fe8027f31e9d3bffcc94b966debd802f32
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-01-21 11:16:07 +1100

    added unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/dan/www/mytruckfleet"
+pid "/home/dan/www/mytruckfleet/tmp/pids/unicorn.pid"
+stperr_path "/home/dan/www/mytruckfleet/log/unicorn.log"
+stdout_path "/home/dan/www/mytruckfleet/log/unicorn.log"
+
+listen 3000
+worker_processes 2
+timeout 30
