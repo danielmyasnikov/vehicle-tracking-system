533aa682754c99322b685ae0bb823b0479fed4c2
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-04-16 09:53:29 +1000

    aws settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,9 @@ Mytruckfleet::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => 'my-truck-fleet'
+      :access_key_id => 'AKIAIQLJGUMX2XSY624A'
+      :secret_access_key => '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'
     }
   }
 
