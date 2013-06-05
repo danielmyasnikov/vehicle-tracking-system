e9436b179c14025669e815d6a68c28724b7a79d3
danielmyasnikov <myasnikovdaniil@gmail.com>
2013-06-05 13:05:26 +1000

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130502165955) do
 
-  create_table "animals", :force => true do |t|
-    t.date     "born_on"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "assets", :force => true do |t|
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
@@ -83,9 +77,8 @@ ActiveRecord::Schema.define(:version => 20130502165955) do
     t.string   "mileage"
     t.string   "contact_no"
     t.text     "faults"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "accident_type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
@@ -143,7 +136,6 @@ ActiveRecord::Schema.define(:version => 20130502165955) do
     t.string   "invoice_content_type"
     t.integer  "invoice_file_size"
     t.datetime "invoice_updated_at"
-    t.integer  "age"
   end
 
   create_table "notifications", :force => true do |t|
