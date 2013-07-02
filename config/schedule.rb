4377ff6c530267361822fed97502a038433335bc
danielmyasnikov <myasnikovdaniil@gmail.com>
2013-07-02 12:56:20 +1000

    schedule change for cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :output, "/log/cron_log.log"
-every 1.minutes do
-  command 'bundle exec rake mtf:count_daily_milage RAILS_ENV=production'
+set :output, "/home/dan/www/mytruckfleet/log/cron_log.log"
+every :day, :at => '5:30am' do
+  command 'cd /home/dan/www/mytruckfleet/ && bundle exec rake mtf:count_daily_milage RAILS_ENV=production'
 end
