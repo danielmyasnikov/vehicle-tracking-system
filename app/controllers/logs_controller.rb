3623fdfbd01b6d1db7329e0d3415cc280b395405
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-07-07 22:18:10 +1000

    filter for logo and header

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,11 @@
 class LogsController < ApplicationController
+  before_filter :menu_activization
+  
+  def menu_activization
+    session[:active_menu] = "MyLogs"
+    session[:module_logo] = "" 
+  end
+  
   def index
     @logs = Log.scoped.order("created_at DESC")
   end
