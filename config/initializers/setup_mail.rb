41ab75fee1bf6f33511c49f2de6d190e73bc7d77
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-07-08 11:40:48 +1000

    emails port change

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,6 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :user_name            => "info.mytruckfleet@gmail.com",
   :domain               => "gmail.com",
+  :port                 => "587",
   :password             => "rus9uGap",
   :authentication       => "plain",
   :enable_starttls_auto => true
