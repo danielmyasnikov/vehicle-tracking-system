ec8c44f52e7020802a3159dbead6cd5c2d608caa
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2014-02-05 10:33:56 +1100

    settings stuff

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
-  $('.edit_email_notification select').change(function() {
-    $(this).closest('form').submit();
-  });
-  $('.edit_email_notification input[type=checkbox]').click(function() {
-    $(this).closest('form').submit();
-  });
-  $('.edit_settings select').change(function() {
-    $(this).closest('form').submit();
+    $('.icon-spinner').hide();
+    $('.edit_email_notification select').change(function() {
+        
+        var form_id = $(this).closest('form').attr("id");
+          id = form_id.split("_")[3]
+        var spinner_id = "#spinner-" + id;
+
+        $(spinner_id).show();
+        $(spinner_id).fadeOut(1000);
+        
+        $(this).closest('form').submit();
+    });
+    $('.edit_email_notification input[type=checkbox]').click(function() {
+        var form_id = $(this).closest('form').attr("id");
+            id = form_id.split("_")[3]
+        var spinner_id = "#spinner-" + id;
+    
+        $(spinner_id).show();
+        $(spinner_id).fadeOut(1000);
+    
+        $(this).closest('form').submit();
+    });
+    $('.edit_settings select').change(function() {
+      
+        var form_id = $(this).closest('form').attr("id");
+          id = form_id.split("_")[3]
+        var spinner_id = "#spinner-" + id;
+
+        $(spinner_id).show();
+        $(spinner_id).fadeOut(1000);
+      
+        $(this).closest('form').submit();
   });
 });
